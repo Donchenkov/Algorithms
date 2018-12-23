f588651475d9457ddb562bca00b87071a996cffc
Mikhail Donchenkov <misha.donchenkov@gmail.com>
2018-12-23 13:28:58 +0300

    поправочка

--- lesson_2_hw/task_8.py
+++ lesson_2_hw/task_8.py
@@ -3,14 +3,28 @@
 Количество вводимых чисел и цифра, которую необходимо посчитать, задаются вводом с клавиатуры.
 '''
 
-base = ''
+# base = ''
+# numbers = int(input('Сколько чисел вы хотите ввести:'))
+# dig = int(input('Какую цифру ищем:'))
+#
+#
+# for i in range(numbers):
+#     num = int(input(f'Введите {i+1} число:'))
+#     base += str(num)
+#
+# count = base.count(str(dig))
+# print(f'Количество цифр {dig} в введнных числах: {count}')
+
+count = 0
 numbers = int(input('Сколько чисел вы хотите ввести:'))
 dig = int(input('Какую цифру ищем:'))
 
 
 for i in range(numbers):
-    num = int(input(f'Введите {i+1} число:'))
-    base += str(num)
+    num = int(input(f'Введите {i+1} число: '))
+    while num > 0:
+        if num % 10 == dig:
+            count += 1
+        num = num // 10
 
-count = base.count(str(dig))
 print(f'Количество цифр {dig} в введнных числах: {count}')
